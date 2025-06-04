a53dbd24d37884875e65e5cc0c91e327638010c4
miscuse <205730527+miscuse@users.noreply.github.com>
2025-06-05 00:30:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,10 +26,7 @@ efi_size=${efi_size:-1G}
 
 read -rp "Enter hostname: " hostname
 read -rp "Enter new username: " username
-
-echo -n "[*] Enter a password for root: "
-read -rs password
-echo
+read -rp "Enter a password for root: " password
 
 # --- Step 2: Wipe disk ---
 echo "[*] Wiping disk $disk"
@@ -51,7 +48,7 @@ mkfs.btrfs -L ARCH_ROOT "$root_part"
 
 # --- Step 5: Create subvolumes and mount ---
 echo "[*] Creating and mounting Btrfs subvolumes"
-mount -o compress=zstd,ssd "$root_part" /mnt
+mount -o compress=zstd "$root_part" /mnt
 
 for subvol in root home log cache snapshots-root snapshots-home portables machines aurbuild swap dotvar; do
   btrfs subvolume create "/mnt/$subvol"
@@ -59,25 +56,28 @@ done
 
 umount -R /mnt
 
-mount -o compress=zstd,ssd,subvol=root "$root_part" /mnt
-mkdir -p /mnt/{boot/efi,var/log,var/cache,var/lib/portables,var/lib/machines,var/lib/aurbuild,home/"$username"}
+mount -o compress=zstd,subvol=root "$root_part" /mnt
+mkdir -p /mnt/{boot/efi,var/log,var/cache,var/lib/portables,var/lib/machines,var/lib/aurbuild,swap,home/"$username"}
 mount "$efi_part" /mnt/boot/efi
-mount -o compress=zstd,ssd,subvol=log "$root_part" /mnt/var/log
-mount -o compress=zstd,ssd,subvol=cache "$root_part" /mnt/var/cache
-mount -o compress=zstd,ssd,subvol=portables "$root_part" /mnt/var/lib/portables
-mount -o compress=zstd,ssd,subvol=machines "$root_part" /mnt/var/lib/machines
-mount -o compress=zstd,ssd,subvol=aurbuild "$root_part" /mnt/var/lib/aurbuild
-mount -o compress=zstd,ssd,subvol=home "$root_part" /mnt/home/"$username"
+mount -o compress=zstd,subvol=log "$root_part" /mnt/var/log
+mount -o compress=zstd,subvol=cache "$root_part" /mnt/var/cache
+mount -o compress=zstd,subvol=portables "$root_part" /mnt/var/lib/portables
+mount -o compress=zstd,subvol=machines "$root_part" /mnt/var/lib/machines
+mount -o compress=zstd,subvol=aurbuild "$root_part" /mnt/var/lib/aurbuild
+mount -o compress=zstd,subvol=swap "$root_part" /mnt/swap
+mount -o compress=zstd,subvol=home "$root_part" /mnt/home/"$username"
+mkdir -p /mnt/{.snapshots,home/"$username"/.snapshots}
+mount -o compress=zstd,subvol=snapshots-root "$root_part" /mnt/.snapshots
+mount -o compress=zstd,subvol=snapshots-home "$root_part" /mnt/home/"$username"/.snapshots
 mkdir -p /mnt/home/"$username"/.var
-mount -o compress=zstd,ssd,subvol=dotvar "$root_part" /mnt/home/"$username"/.var
+mount -o compress=zstd,subvol=dotvar "$root_part" /mnt/home/"$username"/.var
 
 # --- Step 6: Install base system ---
 echo "[*] Installing base system"
 pacstrap -K /mnt base base-devel linux linux-firmware \
-  neovim networkmanager sudo btrfs-progs man-db man-pages \
-  vim intel-ucode zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting \
-  noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-sarasa-gothic ttf-jetbrains-mono \
-  ttf-nerd-fonts-symbols-mono starship htop moreutils ripgrep fd fzf fastfetch
+  vim neovim networkmanager sudo btrfs-progs man-db man-pages \
+  zsh moreutils pacman-contrib htop ripgrep fd fzf fastfetch \
+  grub efibootmgr os-prober
 
 # --- Step 7: Configure system ---
 echo "[*] Generating fstab"
@@ -102,25 +102,66 @@ echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo "root:$password" | chpasswd
 useradd -m -G wheel -s /bin/zsh "$username"
 echo "$username:$password" | chpasswd
-cp -rT /etc/skel /home/$username
-chown -R "$username:$username" /home/$username
+cp -rT /etc/skel /home/"$username"
+chown -R "$username:$username" /home/"$username"
 
 # Enable sudo for wheel group
 sed -i 's/^\(#\s*\)\(%wheel ALL=(ALL:ALL) ALL.*\)/\2/' /etc/sudoers
 
 # GRUB installation
-pacman -S --noconfirm grub efibootmgr os-prober
 sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=3/' /etc/default/grub
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 button.lid_init_state=open"/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install other packages
-pacman -S --noconfirm gnome firefox firefox-i18n-zh-cn
+# Install packages
+pacman -S --noconfirm --asdeps gnome
+pacman -S --noconfirm firefox firefox-i18n-zh-cn
+pacman -S --noconfirm zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+pacman -S --noconfirm noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-sarasa-gothic ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono
+pacman -S --noconfirm snapper snap-pac
+pacman -S --noconfirm chrony cpupower chezmoi starship direnv
 
-# Fix nvidia black screen issue (optional)
+# docker & virt-manager
+pacman -S --noconfirm docker virt-manager
+pacman -S --noconfirm --asdeps dnsmasq qemu-desktop
+usermod -aG docker,libvirt "$username"
+
+# archlinuxcn packages
+cat << EOF > /etc/pacman.d/cnmirrorlist
+CacheServer = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.cloud.tencent.com/archlinuxcn/$arch
+CacheServer = https://mirrors.jlu.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.nju.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.zju.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.pku.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+CacheServer = https://mirrors.163.com/archlinux-cn/$arch
+#CacheServer = https://mirrors.xtom.hk/archlinuxcn/$arch
+#Server = https://mirrors.xtom.hk/archlinuxcn/$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
+Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+EOF
+
+cat << EOF >> /etc/pacman.conf
+[archlinuxcn]
+Include = /etc/pacman.d/cnmirrorlist
+EOF
+
+pacman -Syu --noconfirm archlinuxcn-keyring paru
+
+############ hardware specific ############
+# nvidia
+pacman -S --noconfirm nvidia-open
+systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 ln -sf /dev/null /etc/udev/rules.d/61-gdm.rules
+
+# intel-ucode
+pacman -S --noconfirm intel-ucode
+###########################################
 
 # Enable services
 systemctl enable NetworkManager
